d355fd25e3fb399318d254816ef05439bf1e23d7
a <a>
2025-03-16 22:09:26 -0700

    Added Test to Cell.h

--- Cell.h
+++ Cell.h
@@ -1,6 +1,11 @@
 #include <string>
 #include <iostream>
-using namespace std;
+
+void die(const std::string &s) {
+    std::cout << s << std::endl;
+    exit(1);    
+}
+
 
 class Cell {
 public:
@@ -13,29 +18,35 @@ public:
         prev = newPrev;
         next = newNext;
     }
+};
+	std::string CellTester() {
+        //std::cout << "This test will check to see if the Cell objects work as intended (a node in a doubly linked list)." << std::endl;
 
-    void CellTester() {
-        cout << "This test will check to see if the Cell objects work as intended (a node in a doubly linked list)." << endl;
-
-        cout << "adding the first node..." << endl;
+        //std::cout << "adding the first node..." << std::endl;
         Cell* firstNode = new Cell("First node");
+        if (firstNode->particle != "First node") die("One parameter constructor is broken.");
     
-        cout << "adding the second node..." << endl;
+        //std::cout << "adding the second node..." << std::endl;
         Cell* secondNode = new Cell("Second node", firstNode);
         firstNode->next = secondNode; //make firstnode point to secondnode now that it exists
+        if (firstNode->next != secondNode) die("Setting the next pointer value of a node to another node does not work.");
 
-        cout << "printing out the sequence of two nodes:" << endl;
-        cout << firstNode->particle << endl;
-        cout << "again, but from secondNode's pointer: " << secondNode->prev->particle << endl;
-        cout << firstNode->next->particle << endl;
+        //std::cout << "printing out the sequence of two nodes:" << std::endl;
+        //std::cout << firstNode->particle << std::endl;
+        //std::cout << "again, but from secondNode's pointer: " << secondNode->prev->particle << std::endl;
+        if (secondNode->prev->particle != "First node") die("Retrieving the string in the previous node using another node as reference did not work.");
+		if (firstNode->next->particle != "Second node") die("Pointer from the first node to the second node was not set.");
+        //std::cout << firstNode->next->particle << std::endl;
 
-        cout << "adding the third and final node..." << endl;
+        //std::cout << "adding the third and final node..." << std::endl;
         Cell* thirdNode = new Cell("Third node", nullptr, firstNode);
         firstNode->prev = thirdNode;
+        if (secondNode->prev->prev->particle != "firstNode") die("could read the first item using the last item as reference.");
+        //std::cout << "Outputting the completed list:" << std::endl;
+        //for (Cell* temp = thirdNode; temp; temp = temp->next) 
+            //std::cout << temp->particle << std::endl;
 
-        cout << "Outputting the completed list:" << endl;
-        for (Cell* temp = thirdNode; temp; temp = temp->next) 
-            cout << temp->particle << endl;
+        //}
+		return "Cell.h passed all tests!";
+		}
 
-        }
-};
