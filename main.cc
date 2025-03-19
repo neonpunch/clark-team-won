ca36a8d223a7e296fedfc00a6138260f030521f6
clark_0762647 <0762647@my.scccd.edu>
2025-03-19 12:57:59 -0700

    Trying to break shit

--- main.cc
+++ main.cc
@@ -1,19 +1,14 @@
 #include <ostream>
 #include "System.h"
-#include "Cell.h" 
-#include "Particle.h"
-#include "Graphics.h"
 using namespace std;
 
 int main () {
 	System s;
-	s.testSystem();
-
-	cout << testinggc() << endl;
-
-	Particle p;
-	p.testParticle();
 	
-	cout << CellTester() << endl;
+	Particle p;
+
+	//s.addParticle(p);
+
+	s.sysUpdate();
 
 }
