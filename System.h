7972e0811cc3f39ef288f783bbcd76d65500b31b
neonpunch <0762647@my.scccd.edu>
2025-03-21 14:08:20 -0700

    Update System.h

--- System.h
+++ System.h
@@ -71,60 +71,60 @@ public:
         return partCount; // Updated to return actual particle count
     }
 
-void sysUpdate() {
-    Cell *currNode = head;
-    while (currNode) {
-        Cell *nextNode = currNode->getNext(); // Save the next node before potentially deleting the current node
-        currNode->particle.Physics();
-        double x = currNode->particle.get_x();
-        double y = currNode->particle.get_y();
-        if ((x < 0 or x > scrnWidth) or (y < 0 or y > scrnHeight) or currNode->particle.get_lifetime() == 0) {
-            if (currNode->prev) currNode->prev->next = currNode->next;
-            if (currNode->next) currNode->next->prev = currNode->prev;
-            if (currNode == head) head = currNode->next;
-            if (currNode == tail) tail = currNode->prev;
-            delete currNode;
-            partCount--;
-        } else {
-            drawParticle(currNode->particle);
+    void sysUpdate() {
+        Cell *currNode = head;
+        while (currNode) {
+            Cell *nextNode = currNode->getNext(); // Save the next node before potentially deleting the current node
+            currNode->particle.Physics(*this); // Pass system to Physics
+            double x = currNode->particle.get_x();
+            double y = currNode->particle.get_y();
+            if ((x < 0 or x > scrnWidth) or (y < 0 or y > scrnHeight) or currNode->particle.get_lifetime() == 0) {
+                if (currNode->prev) currNode->prev->next = currNode->next;
+                if (currNode->next) currNode->next->prev = currNode->prev;
+                if (currNode == head) head = currNode->next;
+                if (currNode == tail) tail = currNode->prev;
+                delete currNode;
+                partCount--;
+            } else {
+                drawParticle(currNode->particle);
+            }
+            currNode = nextNode; // Move to the next node
         }
-        currNode = nextNode; // Move to the next node
     }
-}
 
     void drawParticle(const Particle &p) {
         g.drawPoint(p.get_y(), p.get_x(), p.get_r(), p.get_g(), p.get_b());
     }
 
-  void testSystem() {
-    System s;
-    bool passed = true;
+    void testSystem() {
+        System s;
+        bool passed = true;
 
-    if (s.get_head() != nullptr) {
-        cout << "Head bad" << endl;
-        passed = false;
-    } 
+        if (s.get_head() != nullptr) {
+            cout << "Head bad" << endl;
+            passed = false;
+        } 
 
-    if (s.get_tail() != nullptr) {
-        cout << "Tail bad" << endl;
-        passed = false;
-    }
+        if (s.get_tail() != nullptr) {
+            cout << "Tail bad" << endl;
+            passed = false;
+        }
 
-    if (s.get_partCount() != 0) {
-        cout << "PartCount bad" << endl;
-        passed = false;
-    } 
+        if (s.get_partCount() != 0) {
+            cout << "PartCount bad" << endl;
+            passed = false;
+        } 
 
-    s.set_partCount(666);
-    if (s.get_partCount() != 666) {
-        cout << "Setting or getting partCount is bad" << endl;
-        passed = false;
-    }
+        s.set_partCount(666);
+        if (s.get_partCount() != 666) {
+            cout << "Setting or getting partCount is bad" << endl;
+            passed = false;
+        }
 
-    if (passed) {
-        cout << "System.h passed all tests!" << endl;
-    } else {
-        cout << "System.h did not pass all tests." << endl;
-    }
-  }  
+        if (passed) {
+            cout << "System.h passed all tests!" << endl;
+        } else {
+            cout << "System.h did not pass all tests." << endl;
+        }
+    }    
 };
