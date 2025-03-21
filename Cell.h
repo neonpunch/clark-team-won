9efadc7ab3c1a98664c6ba40e5915e464046ddc1
neonpunch <0762647@my.scccd.edu>
2025-03-21 13:06:19 -0700

    updated testcell

--- Cell.h
+++ Cell.h
@@ -41,45 +41,43 @@ public:
 	}
 
 };
-	void testCell() {
-	//return "CellTester currently does not account for the Particle object type. Please contact me (Parker) if testing is needed for the Cell class in the future.";
-		
-		bool passed = true;
-		
-        Cell* firstNode = new Cell(Particle());
-        if (firstNode->particle != Particle(0,0,0,0,0,5,5,5)) {
-			cout << "Accessing a particle is broken." << endl;
-			passed = false;
-		}
-    
-        Cell* secondNode = new Cell(Particle(12,15,0,0,4,7,8,9), firstNode);
-        firstNode->next = secondNode; //make firstnode point to secondnode now that it exists
-        if (firstNode->next != secondNode) {
-			cout << "Setting the next pointer value of a node to another node does not work." << endl;
-			passed = false;
-		}
+void testCell() {
+    bool passed = true;
 
-        if (secondNode->prev->particle != Particle(0,0,0,0,0)) {
-			cout << "Retrieving the string in the previous node using another node as reference did not work." << endl;
-			passed = false;
-		}
+    Cell* firstNode = new Cell(Particle());
+    if (!(firstNode->particle == Particle(0,0,0,0,0,5,5,5))) {
+        cout << "Accessing a particle is broken." << endl;
+        passed = false;
+    }
 
-		if (firstNode->next->particle != Particle(12,15,0,0,4,7,8,9)) {
-			cout << "Pointer from the first node to the second node was not set." << endl;
-			passed = false;
-		}
+    Cell* secondNode = new Cell(Particle(12,15,0,0,4,7,8,9), firstNode);
+    firstNode->next = secondNode; //make firstnode point to secondnode now that it exists
+    if (firstNode->next != secondNode) {
+        cout << "Setting the next pointer value of a node to another node does not work." << endl;
+        passed = false;
+    }
 
-        Cell* thirdNode = new Cell(Particle(30,7,0,0,15,12,0,6), nullptr, firstNode);
-        firstNode->prev = thirdNode;
-        if (secondNode->prev->prev->particle != Particle(30,7,0,0,15,12,0,6)) {
-			cout << "Accessing two particles before does not work." << endl;
-			passed = false;
-		}
+    if (!(secondNode->prev->particle == Particle(0,0,0,0,0))) {
+        cout << "Retrieving the particle in the previous node using another node as reference did not work." << endl;
+        passed = false;
+    }
 
-		delete firstNode;
-		delete secondNode;
-		delete thirdNode;
-		
-		if (passed) cout << "Cell.h passed all tests!" << endl;
-		else cout << "Cell.h did not pass all tests." << endl;
-	}
+    if (!(firstNode->next->particle == Particle(12,15,0,0,4,7,8,9))) {
+        cout << "Pointer from the first node to the second node was not set." << endl;
+        passed = false;
+    }
+
+    Cell* thirdNode = new Cell(Particle(30,7,0,0,15,12,0,6), nullptr, firstNode);
+    firstNode->prev = thirdNode;
+    if (!(secondNode->prev->prev->particle == Particle(30,7,0,0,15,12,0,6))) {
+        cout << "Accessing two particles before does not work." << endl;
+        passed = false;
+    }
+
+    delete firstNode;
+    delete secondNode;
+    delete thirdNode;
+
+    if (passed) cout << "Cell.h passed all tests!" << endl;
+    else cout << "Cell.h did not pass all tests." << endl;
+}
