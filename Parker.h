d302ac109c8d64a3c237f99025480dae21700162
a <a>
2025-03-25 19:40:11 -0700

    Added lifetime variable and Gas enum properly

--- Parker.h
+++ Parker.h
@@ -2,13 +2,13 @@
 
 //Parker's gas project
 
-static void GasClouds(System &Gas) {
-	int verticalThird = Gas.get_scrnHeight() / 3;
-	int horizontalFifth = Gas.get_scrnWidth() / 5;
+static void GasClouds(System &Controller) {
+	int verticalThird = Controller.get_scrnHeight() / 3;
+	int horizontalFifth = Controller.get_scrnWidth() / 5;
 	//make all the particles at random parts near the bottom, maybe make smokestacks another day 
 	for (int i = 0; i < 120; i++) {
-		Gas.addParticle(Particle( ( horizontalFifth + (rand() % (3 * horizontalFifth))  ), ( (2 * verticalThird) + (rand() % verticalThird)  ) , 0 , (((rand() % 5) + 1) / 5), 500, 50 + (rand() % 15) - 7, 200 + (rand() % 56), 20, 5));
-		Gas.sysUpdate();	
+		Controller.addParticle(Particle( ( horizontalFifth + (rand() % (3 * horizontalFifth))  ), ( (2 * verticalThird) + (rand() % verticalThird)  ) , 0 , (((rand() % 5) + 1) / 5), 500, 50 + (rand() % 15) - 7, 200 + (rand() % 56), 20, Particle::GAS));
+		Controller.sysUpdate();	
 	}
 }
 
