b5d07a15177796f9124a0a4d4e3c570b818b3aea
a <a>
2025-03-25 19:08:37 -0700

    Added my gas simulation function

--- Parker.h
+++ Parker.h
@@ -1,3 +1,17 @@
 #include "System.h"
 
 //Parker's gas project
+
+static void parker_gasParticle() {
+	System Gas;
+	int verticalThird = Gas.get_scrnHeight() / 3;
+	int horizontalFifth = Gas.get_scrnWidth() / 5;
+	//make all the particles at random parts near the bottom, maybe make smokestacks another day 
+	for (int i = 0; i < 120; i++) {
+		Sys.addParticle(Particle( ( horizontalFifth + (rand() % (3 * horizontalFifth))  ), ( (2 * verticalThird) + (rand() % verticalThird)  ) , 0 , (((rand() % 5) + 1) / 5), 50 + (rand() % 15) - 7, 200 + (rand() % 56), 20, GAS));
+		Sys.sysUpdate();	
+	}
+}
+
+
+
