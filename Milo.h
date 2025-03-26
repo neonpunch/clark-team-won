77135af14d63a7efa71be7b5482619442b9d8859
Joshua Clark <0762647@my.scccd.edu>
2025-03-25 17:43:31 -0700

    Update Milo.h

--- Milo.h
+++ Milo.h
@@ -37,7 +37,6 @@ void mouse_handler(int row, int col) {
     click_row = row;
     click_col = col;
     mouse_down = true;
-    cout << "Mouse down at (" << click_col << ", " << click_row << ")" << endl;
 }
 
 // Mouse up handler
@@ -45,7 +44,6 @@ void mouse_handler2(int row, int col) {
     click_row = row;
     click_col = col;
     mouse_down = false;
-    cout << "Mouse up at (" << click_col << ", " << click_row << ")" << endl;
 }
 
 // Function to draw particles
@@ -68,22 +66,6 @@ void BitBomb(System& system) {
 
     const auto [ROWS, COLS] = get_terminal_size();
     cout << "Terminal size: " << ROWS << " rows, " << COLS << " columns" << endl;
-
-    for (int y = 0; y < ROWS; y++) {
-        for (int x = 0; x < COLS; x++) {
-            Particle wallParticle(x, y, 0, 0, INT_MAX, 255, 255, 255, Particle::STREAMER);
-            system.addParticle(wallParticle);
-            draw_particle(wallParticle);
-        }
-    }
-
-    vector<Particle> catArtParticles;
-    for (int y = ROWS / 2 - 5; y < ROWS / 2 + 5; y++) {
-        for (int x = COLS / 2 - 5; x < COLS / 2 + 5; x++) {
-            Particle artParticle(x, y, 0, 0, INT_MAX, 255, 165, 0, Particle::BALLISTIC);
-            catArtParticles.push_back(artParticle);
-        }
-    }
 
     auto last_time = high_resolution_clock::now();
 
@@ -108,61 +90,12 @@ void BitBomb(System& system) {
             mouse_down = false;
             if (click_row >= ROWS or click_col >= COLS or click_row < 0 or click_col < 0) continue;
             cout << "Mouse click at (" << click_col << ", " << click_row << ")" << endl;
-            Particle explosiveParticle(click_col, click_row, 0, 0, 100, 255, 0, 0, Particle::FIREWORK);
-            system.addParticle(explosiveParticle);
-            draw_particle(explosiveParticle);
+            Particle drawParticle(click_col, click_row, 0, 0, INT_MAX, rand() % 256, rand() % 256, rand() % 256, Particle::BALLISTIC);
+            system.addParticle(drawParticle);
+            draw_particle(drawParticle);
         }
 
         system.sysUpdate();
-
-        for (Cell* curr = system.get_head(); curr != nullptr; curr = curr->getNext()) {
-            Particle tempParticle = curr->getParticle();
-            if (tempParticle.get_type() == Particle::FIREWORK && tempParticle.get_lifetime() == 0) {
-                double exX = tempParticle.get_x();
-                double exY = tempParticle.get_y();
-                for (Cell* wallCurr = system.get_head(); wallCurr != nullptr; wallCurr = wallCurr->getNext()) {
-                    Particle tempWallParticle = wallCurr->getParticle();
-                    double wx = tempWallParticle.get_x();
-                    double wy = tempWallParticle.get_y();
-                    if (sqrt(pow(wx - exX, 2) + pow(wy - exY, 2)) < 10.0) {
-                        wallCurr->getParticle().set_lifetime(0);
-                        draw_particle(wallCurr->getParticle());
-                    }
-                }
-            }
-        }
-
-        int destroyedCount = 0;
-        for (Cell* curr = system.get_head(); curr != nullptr; curr = curr->getNext()) {
-            Particle tempParticle = curr->getParticle();
-            if (tempParticle.get_lifetime() == 0 && tempParticle.get_type() == Particle::STREAMER) {
-                destroyedCount++;
-            }
-        }
-
-        if (destroyedCount > (ROWS * COLS / 2)) {
-            for (Particle& artParticle : catArtParticles) {
-                system.addParticle(artParticle);
-                draw_particle(artParticle);
-            }
-            for (Cell* curr = system.get_head(); curr != nullptr; curr = curr->getNext()) {
-                Particle tempParticle = curr->getParticle();
-                if (tempParticle.get_type() == Particle::STREAMER) {
-                    curr->getParticle().set_lifetime(0);
-                    draw_particle(curr->getParticle());
-                }
-            }
-            for (int i = 0; i < 50; i++) {
-                double angle = (rand() % 360) * (numbers::pi / 180.0);
-                double speed = 2.0;
-                double new_dx = speed * cos(angle);
-                double new_dy = speed * sin(angle);
-                Particle finalExplosionParticle(COLS / 2, ROWS / 2, new_dx, new_dy, INT_MAX, rand() % 256, rand() % 256, rand() % 256, Particle::FIREWORK);
-                system.addParticle(finalExplosionParticle);
-                draw_particle(finalExplosionParticle);
-            }
-            break;
-        }
 
         usleep(100000); // Sleep for 100 milliseconds
     }
