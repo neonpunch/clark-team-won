e4f1bd31ee85137642a1c1c8c6af30436be27974
Joshua Clark <0762647@my.scccd.edu>
2025-03-25 18:29:44 -0700

    Update Milo.h

--- Milo.h
+++ Milo.h
@@ -60,11 +60,12 @@ void BitBomb(System& system) {
         if (rand() % 10 == 0) { // Consistent rate
             int firework_x = rand() % COLS;
             int firework_y = ROWS - 1; // Start from the bottom of the screen
+            int explosion_y = rand() % (ROWS / 2); // Random explosion point within the screen
             Particle explosiveParticle(firework_x, firework_y, 0, -1, 100, 255, 0, 0, Particle::FIREWORK); // Move upwards
             system.addParticle(explosiveParticle);
 
-            // Move the firework up and then explode
-            while (explosiveParticle.get_y() > 0) {
+            // Move the firework up and then explode at the random explosion point
+            while (explosiveParticle.get_y() > explosion_y) {
                 explosiveParticle.set_y(explosiveParticle.get_y() - 1);
                 system.drawParticle(explosiveParticle);
                 usleep(50000); // Sleep for 50 milliseconds
@@ -76,7 +77,7 @@ void BitBomb(System& system) {
                 double speed = 2.0;
                 double new_dx = speed * cos(angle);
                 double new_dy = speed * sin(angle);
-                Particle explosionParticle(explosiveParticle.get_x(), explosiveParticle.get_y(), new_dx, new_dy, INT_MAX, rand() % 256, rand() % 256, rand() % 256, Particle::FIREWORK);
+                Particle explosionParticle(explosiveParticle.get_x(), explosion_y, new_dx, new_dy, INT_MAX, rand() % 256, rand() % 256, rand() % 256, Particle::FIREWORK);
                 system.addParticle(explosionParticle);
                 system.drawParticle(explosionParticle);
             }
